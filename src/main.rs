647dd4d191f5f5820b9c520cace972358e544967
Igor Guerrero <igorgue@protonmail.com>
2022-12-31 02:58:27 -0600

    to be continued

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use actix_web::{get, middleware, post, web, App, HttpResponse, HttpServer, Responder};
-use env_logger;
 use log::info;
 use std::env;
 
@@ -25,46 +24,41 @@ fn handle_args(args: Vec<String>) -> std::io::Result<()> {
 
     if args[1] == "fetch-data" {
         info!("Fetching data...");
-
-        // TODO: Run fetch new items here
-
-        Ok(())
     } else {
-        info!("Invalid arguments");
-
-        Ok(())
+        info!("Invalid argument: {}", args[1]);
     }
-}
 
+    Ok(())
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+    env_logger::init_from_env::<_>(env_logger::Env::default().default_filter_or("info"));
 
     let args: Vec<String> = env::args().collect();
 
     if args.len() >= 2 {
-        handle_args(args)
-    } else {
-        let is_ssl = env::var("SSL").is_ok();
-        let host = env::var("HOST").unwrap_or_else(|_| HOST.to_string());
-        let port = env::var("PORT")
-            .unwrap_or_else(|_| PORT.to_string())
-            .parse::<u16>()
-            .unwrap_or(PORT);
-        let protocol = if is_ssl { "https" } else { "http" };
-
-        info!("Starting server at {}://{}:{}", protocol, host, port);
-
-        HttpServer::new(|| {
-            App::new()
-                .service(hello)
-                .service(echo)
-                .wrap(middleware::Logger::default())
-                .route("hey", web::get().to(manual_hello))
-        })
-        .bind((host, port))?
-        .run()
-        .await
+        return handle_args(args);
     }
+
+    let is_ssl = env::var("SSL").is_ok();
+    let host = env::var("HOST").unwrap_or_else(|_| HOST.to_string());
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| PORT.to_string())
+        .parse::<u16>()
+        .unwrap_or(PORT);
+    let protocol = if is_ssl { "https" } else { "http" };
+
+    info!("Starting server at {}://{}:{}", protocol, host, port);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .wrap(middleware::Logger::default())
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind((host, port))?
+    .run()
+    .await
 }
