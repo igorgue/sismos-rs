d0b9565a9e0b0faf151c5340f0af14f1badcde44
Igor Guerrero <igorgue@protonmail.com>
2023-01-27 18:11:32 -0600

    wip

--- tests/api_test.rs
+++ tests/api_test.rs
@@ -1,15 +1,21 @@
 use std::env;
+
 use actix_web::{test, App};
-use sismos::api::root;
+use ctor;
+use sismos::{api::root, models::SismoResponse};
+
+#[ctor::ctor]
+fn init() {
+    env::set_var("DATABASE_URL", "sqlite://tests/data/sismos.test.db");
+}
 
 #[actix_web::test]
 async fn test_root() {
-    env::set_var("DATABASE_URL", "sqlite://tests/data/sismos.test.db");
-    let app = test::init_service(
-        App::new().service(root)
-    ).await;
+    let app = test::init_service(App::new().service(root)).await;
     let req = test::TestRequest::get().uri("/").to_request();
-    let resp = test::call_service(&app, req).await;
+    let resp = test::call_and_read_body(&app, req).await;
 
-    assert_eq!(resp.status(), 200);
+    println!("{:?}", resp);
+
+    // assert_eq!(resp.status(), 200);
 }
